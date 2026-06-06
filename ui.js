f6e69693fc8e1cdd5ffeecbef7bc4d69c9bf5a7e
kjforfun <o912779665@gmail.com>
2026-06-07 05:49:43 +0800

    Update ui.js

--- ui.js
+++ ui.js
@@ -214,7 +214,7 @@ rows.forEach(r => {
       .slice(0, 3)
       .map(([combo, data], idx) => {
 
-        const parts = combo.split(' ');
+        const [blade, setup] = combo.split('|');
         const blade = parts[0];
         const setup = parts.slice(1).join(' ');
 
